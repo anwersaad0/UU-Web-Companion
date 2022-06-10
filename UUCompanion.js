c3a768d749d0230b43f8d42dcef3b82727a4ae1b
anwersaad0 <80143838+anwersaad0@users.noreply.github.com>
2022-06-09 20:22:58 -0700

    Added all necessary features
    All the features I have planned to add into the project are added.
    
    - Create Player
    - Start Game
    - Reset Game
    - Reset All
    - Adding Cards
    - Removing Cards
    - Removing Players
    - Changing Turns

--- UUCompanion.js
+++ UUCompanion.js
@@ -1,34 +1,42 @@
 let playerList = [];
+const MAX_PLAYERS = 8;
 
 // HTML imports
 const playerListDiv = document.getElementById("playerListDiv");
+
 const playerModal = document.getElementById("playerModal");
-const cardModal = document.getElementById("cardModal");
+const startGameModal = document.getElementById("startGameModal");
+const resetGameModal = document.getElementById("resetGameModal");
+const resetAllModal = document.getElementById("resetAllModal");
 
 const addPlayerBtn = document.getElementById("addPlayer");
 const startGameBtn = document.getElementById("startGame");
 const resetGameBtn = document.getElementById("resetGame");
-const resetAllBtn = document.getElementById("resetAll");
+const resetAllBtn = document.getElementById("resetAllBtn");
 
 const playerNameInput = document.getElementById("playerName");
 const submitPlayerBtn = document.getElementById("submitPlayer");
 
+const startGameInput = document.getElementById("startGameName");
+const submitStartGame = document.getElementById("submitStart");
+
 const cardNameInput = document.getElementById("cardName");
 const cardDescInput = document.getElementById("cardDesc");
 const submitCardBtn = document.getElementById("submitCard");
+
+const declineResetGameBtn = document.getElementById("declineResetGame");
+const confirmResetGameBtn = document.getElementById("confirmResetGame");
+
+const declineResetAllBtn = document.getElementById("declineResetAll");
+const confirmResetAllBtn = document.getElementById("confirmResetAll");
 //
 
 //constructors
-function Player(name, stable, ifTurn, playerDiv) {
+function Player(name, stable, playerDiv, cardsDiv) {
     this.name = name;
     this.stable = stable;
-    this.ifTurn = ifTurn;
     this.playerDiv = playerDiv;
-}
-
-function Card(name, description) {
-    this.name = name;
-    this.description = description;
+    this.cardsDiv = cardsDiv;
 }
 //
 
@@ -39,8 +47,6 @@ function addPlayer() {
 
     let newStable = [];
 
-    let newIfTurn = false;
-
     let newPlayerDiv = document.createElement("div");
     newPlayerDiv.setAttribute("id", "playerDiv");
     playerListDiv.appendChild(newPlayerDiv);
@@ -49,14 +55,47 @@ function addPlayer() {
     playerUI.setAttribute("id", "playerUI");
 
     let cardsLabel = document.createElement("p");
-    cardsLabel.textContent = "Cards";
+    cardsLabel.textContent = "Stable";
     cardsLabel.setAttribute("id", "cardsLabel");
 
     let addCardBtn = document.createElement("button");
     addCardBtn.setAttribute("id", "addCardBtn");
     addCardBtn.textContent = "Add Card";
     addCardBtn.addEventListener('click', function(e) {
-        cardModal.style.display = "block";
+        
+        let addedCardDiv = document.createElement("div");
+        addedCardDiv.setAttribute("id", "addedCardDiv");
+        newCardsDiv.appendChild(addedCardDiv);
+
+        let chooseCardName = document.createElement("input");
+        addedCardDiv.appendChild(chooseCardName);
+
+        let chooseCardDesc = document.createElement("input");
+        addedCardDiv.appendChild(chooseCardDesc);
+
+        let submitCardBtn = document.createElement("button");
+        submitCardBtn.textContent = "Submit Card";
+        submitCardBtn.addEventListener('click', function(e) {
+            if (chooseCardName.value != "") {
+                chooseCardName.disabled = true;
+
+                if (chooseCardDesc.value == "") {
+                    chooseCardDesc.value = "No Effect";
+                }
+                chooseCardDesc.disabled = true;
+
+                deleteCardBtn.style.display = "inline";
+                addedCardDiv.removeChild(submitCardBtn);
+            }
+        });
+
+        let deleteCardBtn = document.createElement("button");
+        deleteCardBtn.textContent = "Delete Card";
+        deleteCardBtn.onclick = function(){removeCard(addedCardDiv, newCardsDiv)};
+        deleteCardBtn.style.display = "none";
+
+        addedCardDiv.appendChild(submitCardBtn);
+        addedCardDiv.appendChild(deleteCardBtn);
     })
 
     let deletePlayerBtn = document.createElement("button");
@@ -67,87 +106,118 @@ function addPlayer() {
     let nextTurnButton = document.createElement("button");
     nextTurnButton.setAttribute("id", "nextTurnBtn");
     nextTurnButton.textContent = "Next Turn";
+    nextTurnButton.onclick = function(){nextTurn(newPlayerDiv)};
 
-    let cardsDiv = document.createElement("div");
-    cardsDiv.setAttribute("id", "cardsDiv");
-
-    const newPlayer = new Player(playerNameInput.value, newStable, newIfTurn, newPlayerDiv);
+    let newCardsDiv = document.createElement("div");
+    newCardsDiv.setAttribute("id", "cardsDiv");
 
     playerUI.appendChild(cardsLabel);
     playerUI.appendChild(addCardBtn);
     playerUI.appendChild(nextTurnButton);
 
     newPlayerDiv.appendChild(newPlayerName);
-    //newPlayerDiv.appendChild(addCardBtn);
     newPlayerDiv.appendChild(playerUI);
-    newPlayerDiv.appendChild(cardsDiv);
+    newPlayerDiv.appendChild(newCardsDiv);
     newPlayerDiv.appendChild(deletePlayerBtn);
+
+    const newPlayer = new Player(playerNameInput.value, newStable, newPlayerDiv, newCardsDiv);
+    playerList.push(newPlayer);
 }
 
 function removePlayer(selectedDiv, player) {
-    playerListDiv.removeChild(selectedDiv);
-    for (var i = 0; i < playerList; i++) {
+    for (var i = 0; i < playerList.length; i++) {
         if (playerList[i] == player) {
-            playerList.splice(i, i + 1);
+            playerList.splice(i, 1);
         }
     }
-
+    playerListDiv.removeChild(selectedDiv);
 }
 
-function nextTurn() {
-    //switch the border colors of the active player to the next active player
+function beginGame() {
     for (let i = 0; i < playerList.length; i++) {
-        if (playerList[i].ifTurn == true) {
-            playerList[i].ifTurn = false;
-            playerList[i].playerDiv.setAttribute("id, playerDiv");
-            // change player border accordingly
-
-            if (i == playerList.length) {
-                playerlist[0].ifTurn = true;
-                playerList[0].playerDiv.setAttribute("id, currentPlayerDiv");
-            } else {
-                playerlist[i + 1].ifTurn = true;
-                playerList[i + 1].playerDiv.setAttribute("id, currentPlayerDiv");
-            }
-            //change player border accordingly
+        if (playerList[i].name == startGameInput.value) {
+            playerList[i].playerDiv.setAttribute("id", "currentPlayerDiv");
+        } else {
+            playerList[i].playerDiv.setAttribute("id", "playerDiv");
         }
     }
 }
 
-function addCard(card) {
-    //add a card's name and details to a player's stable
+function nextTurn(targetDiv) {
+    for (let i = 0; i < playerList.length; i++) {
+        playerList[i].playerDiv.setAttribute("id", "playerDiv");
+    }
+
+    targetDiv.setAttribute("id", "currentPlayerDiv");
 }
-//
+
+function removeCard(selectedDiv, parent) {
+    parent.removeChild(selectedDiv);
+}
+
 
 addPlayerBtn.addEventListener('click', function(e) {
     playerModal.style.display = "block";
 });
 
-/*function openCardModal() {
-    cardModal.style.display = "block";
-}*/
-
 window.addEventListener('click', function(e) {
     if (e.target == playerModal) {
         playerModal.style.display = "none";
-    } else if (e.target == cardModal) {
-        cardModal.style.display = "none";
+    } else if (e.target == startGameModal) {
+        startGameModal.style.display = "none";
+    } else if (e.target == resetGameModal) {
+        resetGameModal.style.display = "none";
+    } else if (e.target == resetAllModal) {
+        resetAllModal.style.display = "none";
     }
 });
 
 submitPlayerBtn.addEventListener('click', function(e) {
-    if (playerNameInput.value != "") {
+    if (playerNameInput.value != "" && playerList.length < MAX_PLAYERS) {
         addPlayer();
         playerNameInput.value = "";
         playerModal.style.display = "none";
     }
 });
 
-submitCardBtn.addEventListener('click', function(e) {
-    if (cardNameInput.value != "") {
-        //addCard();
-        cardNameInput.value = "";
-        cardDescInput.value = "";
-        cardModal.style.display = "none";
+startGameBtn.addEventListener('click', function(e) {
+    startGameModal.style.display = "block";
+});
+
+resetAllBtn.addEventListener('click', function(e) {
+    resetAllModal.style.display = "block";
+});
+
+resetGameBtn.addEventListener('click', function(e) {
+    resetGameModal.style.display = "block";
+});
+
+confirmResetAllBtn.addEventListener('click', function(e) {
+    playerListDiv.innerHTML = "";
+    playerList = [];
+    resetAllModal.style.display = "none";
+});
+
+declineResetAllBtn.addEventListener('click', function(e) {
+    resetAllModal.style.display = "none";
+});
+
+confirmResetGameBtn.addEventListener('click', function(e) {
+    for (let i = 0; i < playerList.length; i++) {
+        playerList[i].cardsDiv.innerHTML = "";
+        playerList[i].playerDiv.setAttribute("id", "playerDiv");
+    }
+    resetGameModal.style.display = "none";
+});
+
+declineResetGameBtn.addEventListener('click', function(e) {
+    resetGameModal.style.display = "none";
+});
+
+submitStartGame.addEventListener('click', function(e) {
+    if (startGameInput.value != "") {
+        beginGame();
+        startGameInput.value = "";
+        startGameModal.style.display = "none";
     }
 });
